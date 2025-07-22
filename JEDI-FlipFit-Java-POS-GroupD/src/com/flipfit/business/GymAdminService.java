5c6e4ecdd715545a9ba3a73f6fab0da1d16e4365
ronilpatil22 <ronilpatil22@gmail.com>
2025-07-22 16:55:52 +0530

    GymAdmin beans and service

---
+++ JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/business/GymAdminService.java
@@ -0,0 +1,19 @@
+package com.flipfit.business;
+
+public class GymAdminService {
+    public void approveGym(Long approvalId){
+
+    }
+    public void cancelApproval(Long approvalId){
+    }
+    public void viewApprovedList(){
+    }
+    public void addGymOwner(){
+    }
+    public void removeGymOwner(Long GymOwnerId){
+    }
+    public void removeGymCustomer(Long gymCustomerId){
+    }
+    public void removeGym(Long gymId){
+    }
+}
