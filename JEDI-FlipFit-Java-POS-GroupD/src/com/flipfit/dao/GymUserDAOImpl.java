dda204ce9ae3f9ece51733a86d671904e06bf70c
Sajid Anis <sajid.anis20@gmail.com>
2025-07-23 17:05:55 +0530

    UserDAO

--- JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/dao/GymUserDAOImpl.java
+++ JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/dao/GymUserDAOImpl.java
@@ -1,4 +1,18 @@
 package com.flipfit.dao;
 
+import com.flipfit.beans.GymUser;
+import com.flipfit.beans.Role;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class GymUserDAOImpl {
+  private Map<Long, GymUser> userMap = new HashMap<>();
+  private Map<Long, Role> roleMap = new HashMap<>();
+
+  private AtomicLong userId = new AtomicLong();
+  private AtomicLong roleId = new AtomicLong();
+
+
+
 }
