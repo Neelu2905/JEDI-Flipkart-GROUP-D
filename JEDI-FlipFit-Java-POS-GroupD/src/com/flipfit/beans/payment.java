b218242415ffe569f69e80dc2b5a671a7401b448
yerusu.d <yerusu.d@flipkart.com>
2025-07-23 09:18:46 +0530

    Payment

--- JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/beans/payment.java
+++ JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/beans/payment.java
@@ -1,7 +1,24 @@
+package com.flipfit.beans;
 public class payment {
     private customerid;
     private cardNumber;
     private upiId;
-
-
+    public getCustomerid() {
+        return customerid;
+    }
+    public void setCustomerid(customerid) {
+        this.customerid = customerid;
+    }
+    public getCardNumber() {
+        return cardNumber;
+    }
+    public void setCardNumber(cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+    public getUpiId() {
+        return upiId;
+    }
+    public void setUpiId(upiId) {
+        this.upiId = upiId;
+    }
 }
