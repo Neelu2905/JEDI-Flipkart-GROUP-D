913fe454d98d54baa348b7c1d4763e29fb47085b
Sajid Anis <sajid.anis20@gmail.com>
2025-07-23 14:03:16 +0530

    Added Menu

--- JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/utils/Menu.java
+++ JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/utils/Menu.java
@@ -1,7 +1,7 @@
 package com.flipfit.utils;
 
-import com.flipfit.beans.Role;
 import com.flipfit.helper.LoginCredentials;
+import com.flipfit.helper.RegisterData;
 import java.util.Scanner;
 
 public class Menu {
@@ -27,5 +27,63 @@ public class Menu {
     String role = sc.nextLine();
 
     return new LoginCredentials(username, password, role);
+  }
+
+  public static void changePasswordMenu() {
+
+  }
+
+  public static RegisterData registerCustomerMenu() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("\n\n[+] Register for the Gym Customer: ");
+    System.out.print("\t Name: ");
+    String name = sc.nextLine();
+
+    System.out.print("\t Email: ");
+    String email = sc.nextLine();
+
+    System.out.print("\t Password: ");
+    String password = sc.nextLine();
+
+    System.out.println("\t Address: ");
+    String address = sc.nextLine();
+
+    System.out.println("\t Gender: ");
+    String gender = sc.nextLine();
+
+    return new RegisterData.Builder()
+        .setName(name)
+        .setEmail(email)
+        .setPassword(password)
+        .setAddress(address)
+        .setGender(gender)
+        .build();
+  }
+
+  public static RegisterData registerOwnerMenu() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("\n\n[+] Register for the Gym Customer: ");
+    System.out.print("\t Name: ");
+    String name = sc.nextLine();
+
+    System.out.print("\t Email: ");
+    String email = sc.nextLine();
+
+    System.out.print("\t Password: ");
+    String password = sc.nextLine();
+
+    System.out.print("\t Aadhaar Number: ");
+    String aadhar = sc.nextLine();
+
+    System.out.println("\t Pan Number: ");
+    String pan = sc.nextLine();
+
+    return new RegisterData.Builder()
+        .setName(name)
+        .setEmail(email)
+        .setPassword(password)
+        .setAadhar(aadhar)
+        .setPan(pan)
+        .build();
   }
 }
