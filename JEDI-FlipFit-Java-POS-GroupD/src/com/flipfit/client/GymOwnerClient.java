545c7de6ed5e178d3740f52e8e5a03f2b52772ab
ronilpatil22 <ronilpatil22@gmail.com>
2025-07-23 11:04:37 +0530

    GymOwnerClient update

--- JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/client/GymOwnerClient.java
+++ JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/client/GymOwnerClient.java
@@ -1,49 +1,63 @@
 package com.flipfit.client;
 import java.util.Scanner;
+import com.flipfit.beans.GymCentre;
+import com.flipfit.beans.GymOwner;
 import com.flipfit.business.GymOwnerService;
 import com.flipfit.business.GymCentreService;
 import com.flipfit.business.GymUserService;
+
 public class GymOwnerClient {
-    public static void main(String[] args) {
+    public static void GymOwnerMenu() {
         GymOwnerService gymOwnerService = new GymOwnerService();
+        GymOwner gymOwner = new GymOwner();
         GymUserService gymUserService = new GymUserService();
         Scanner in = new Scanner(System.in);
         int option;
 
-        System.out.println("1.Registration");
-        System.out.println("2.Login");
-        System.out.println("3.Gym Centre Registration");
-        System.out.println("4.Edit Slots");
-        System.out.println("5.View Registered Gyms");
-        System.out.println("6.View Registered and free slots");
-        System.out.println("7.Edit Profile");
-        System.out.println("8.Exit");
+        System.out.println("1.Gym Centre Registration");
+        System.out.println("2.Edit Slots");
+        System.out.println("3.View Registered Gyms");
+        System.out.println("4.View Registered and free slots");
+        System.out.println("5.Edit Profile");
+        System.out.println("6.Exit");
         option = in.nextInt();
 
         switch(option){
             case 1:
-                gymUserService.registerUser();
-                break;
-            case 2:
-                gymUserService.loginUser();
-                break;
-            case 3:
                 // Registers a Gym Centre
                 break;
-            case 4:
+            case 2:
                 int gymCenterId;
                 Scanner inputScanner = new Scanner(System.in);
                 System.out.println("Enter Gym Centre Id:");
                 gymCenterId = inputScanner.nextInt();
                 break;
-            case 5:
+            case 3:
                 // Returns a list of registered gyms by the gym owner
                 break;
-            case 6:
+            case 4:
                 // Returns a list of registered and free slots
+
                 break;
-            case 7:
+            case 5:
                 // Edit profile
+                System.out.println("1. Edit name");
+                System.out.println("2. Edit email address");
+
+                int subOption = in.nextInt();
+
+                if(subOption == 1) {
+                    String name = in.next();
+                    gymOwner.setName(name);
+                    System.out.println("Name changed successfully");
+                }
+
+                if(subOption == 2) {
+                    String email = in.next();
+                    gymOwner.setEmail(email);
+                    System.out.println("Email changed successfully");
+                }
+
                 break;
             default:
                 //GymApplicationClient gymApplication = new GymApplicationClient();
