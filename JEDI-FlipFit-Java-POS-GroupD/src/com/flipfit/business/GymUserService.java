90de47e97b33fe39c50836e9dcc5e7e64f430ac5
Sajid Anis <sajid.anis20@gmail.com>
2025-07-23 11:38:40 +0530

    Added dummy login functionality

--- JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/business/GymUserService.java
+++ JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/business/GymUserService.java
@@ -1,9 +1,22 @@
 package com.flipfit.business;
 
+import com.flipfit.exceptions.AuthenticationException;
+import com.flipfit.helper.LoginCredentials;
+
 public class GymUserService {
-  public void registerUser() {
+
+  public boolean login(LoginCredentials loginDetails) throws AuthenticationException {
+    // Connect with Database for further check
+    boolean result = true;
+
+    if(!result){
+      throw new AuthenticationException("Login Failure");
+    }
+
+    return result;
   }
-  public void loginUser() {
+
+  public void registerUser() {
   }
   public void logoutUser() {
   }
