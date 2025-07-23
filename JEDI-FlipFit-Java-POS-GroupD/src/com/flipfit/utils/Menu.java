9bc185627cb396f0d3d70fd1592086029b504671
Sajid Anis <sajid.anis20@gmail.com>
2025-07-23 11:23:25 +0530

    Added Main Application classs

---
+++ JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/utils/Menu.java
@@ -0,0 +1,14 @@
+package com.flipfit.utils;
+
+public class Menu {
+  public static void displayMainMenu() {
+    System.out.println("\n\nWelcome to Flip Fit Application\n");
+
+    System.out.println("[+] Enter your choice:");
+    System.out.println("\t1. Login");
+    System.out.println("\t2. Register for the Gym Customer");
+    System.out.println("\t3. Register for the Gym Owner");
+    System.out.println("\t4. Change Password");
+    System.out.println("\t5. Exit");
+  }
+}
