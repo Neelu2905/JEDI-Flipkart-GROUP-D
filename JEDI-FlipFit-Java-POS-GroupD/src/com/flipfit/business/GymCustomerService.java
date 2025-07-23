2f753ee3b9e1f97e2df187be09e04ccbd04f88f1
Neelu2905 <neeluthegreat99@gmail.com>
2025-07-23 10:32:14 +0530

    created client for customer

--- JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/business/GymCustomerService.java
+++ JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/business/GymCustomerService.java
@@ -6,6 +6,24 @@ import java.util.List;
 
 public class GymCustomerService {
 
+  GymCustomer customer = new GymCustomer();
+
+  public void createCustomer(int id, String name, String emailAddress, int phone, String password) {
+
+    customer.setCustomerEmailAddress(emailAddress);
+    customer.setCustomerName(name);
+    customer.setCustomerId(id);
+    customer.setCustomerPhone(phone);
+    customer.setPassword(password);
+    //Baad me is customer ko database me add karna hai
+
+    System.out.println("customer details added");
+  }
+
+  public String getCustomerDetails(){
+    return null;
+  }
+
   public void viewGymCenters(){
 
   }
--- JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/client/GymCustomerClient.java
+++ JEDI-FlipFit-Java-POS-GroupD/src/com/flipfit/client/GymCustomerClient.java
@@ -1,5 +1,67 @@
+/**
+ *
+ */
 package com.flipfit.client;
 
+import java.util.Scanner;
+
+import com.flipfit.beans.GymCustomer;
+import com.flipfit.business.GymCustomerService;
+
+/**
+ *
+ */
 public class GymCustomerClient {
 
+    public static void customerMenu() {
+
+        GymCustomerService customerService = new GymCustomerService();
+        GymCustomer customer = new GymCustomer();
+        Scanner in = new Scanner(System.in);
+        int option;
+        System.out.println(customerService.getCustomerDetails());
+
+        System.out.println("1. Search Gym");
+        System.out.println("2. View Booked slots");
+        System.out.println("3. Cancel Booking");
+        System.out.println("4. Make Payment");
+        System.out.println("5. Edit Profile");
+        System.out.println("6. Exit");
+
+        option = in.nextInt();
+
+        if(option == 1) {
+            //Search gym according to location
+        }
+        else if(option == 5) {
+            System.out.println("1. Edit name");
+            System.out.println("2. Edit email address");
+            System.out.println("3. Edit phone");
+
+            int subOption = in.nextInt();
+
+            if(subOption == 1) {
+                String name = in.next();
+                customer.setCustomerName(name);
+                System.out.println("Name changed successfully");
+            }
+
+            if(subOption == 2) {
+                String email = in.next();
+                customer.setCustomerEmailAddress(email);
+                System.out.println("Email changed successfully");
+            }
+
+            if(subOption == 3) {
+                int phone = in.nextInt();
+                customer.setCustomerPhone(phone);
+                System.out.println("Contact number changed successfully");
+            }
+
+        }
+
+        else if(option == 6) {
+            //exit and go back to gymapplication page
+        }
+    }
 }
